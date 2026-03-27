49c33e6521426be2162921fbdfea0a37fed36a56
thanhtien2208 <tiennt220805@gmail.com>
2026-03-28 00:57:35 +0700

    feat(ui): Design Accecpt SOS request of Volunteer screen

--- drc-app/app/src/main/java/com/drc/aidbridge/ui/main/fragment/volunteer/VolunteerDashboardFragment.java
+++ drc-app/app/src/main/java/com/drc/aidbridge/ui/main/fragment/volunteer/VolunteerDashboardFragment.java
@@ -1,5 +1,6 @@
 package com.drc.aidbridge.ui.main.fragment.volunteer;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -39,6 +40,8 @@ public class VolunteerDashboardFragment extends BaseFragment<FragmentVolunteerDa
     }
 
     private void setupClickListeners() {
+        String mockNotiType = "RESCUE";
+
         binding.switchOnlineStatus.setOnCheckedChangeListener((buttonView, isChecked) -> {
             updateStatusUI(isChecked);
             showToast(getString(isChecked
@@ -46,28 +49,39 @@ public class VolunteerDashboardFragment extends BaseFragment<FragmentVolunteerDa
                     : com.drc.aidbridge.R.string.volunteer_dashboard_toast_mode_offline));
         });
 
-        binding.cardUserInfo.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_view_profile)));
+        binding.cardUserInfo.setOnClickListener(
+                v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_view_profile)));
 
-        binding.cardCurrentMission.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_open_current_missions)));
+        binding.cardCurrentMission.setOnClickListener(
+                v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_open_current_missions)));
 
-        //binding.cardCompleted.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_open_completed_missions)));
+        // binding.cardCompleted.setOnClickListener(v ->
+        // showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_open_completed_missions)));
         binding.cardCompleted.setOnClickListener(v -> navigateSafely(R.id.action_dashboard_to_history));
 
-        binding.tvSeeAll.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_see_all_notifications)));
+        binding.tvSeeAll.setOnClickListener(
+                v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_see_all_notifications)));
 
-        binding.btnDetails.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_view_mission_detail)));
+        binding.btnDetails.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("missionType", mockNotiType);
+            navigateSafely(R.id.action_dashboard_to_sos_acceptance, bundle);
+        });
 
-        binding.btnLogout.setOnClickListener(v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_logout)));
+        binding.btnLogout.setOnClickListener(
+                v -> showToast(getString(com.drc.aidbridge.R.string.volunteer_dashboard_toast_logout)));
     }
 
     private void updateStatusUI(boolean isOnline) {
         if (isOnline) {
-            binding.tvStatusDescription.setText(com.drc.aidbridge.R.string.volunteer_dashboard_status_online_placeholder);
+            binding.tvStatusDescription
+                    .setText(com.drc.aidbridge.R.string.volunteer_dashboard_status_online_placeholder);
             binding.viewStatusIndicator.setBackgroundResource(com.drc.aidbridge.R.drawable.bg_circle_status_online);
             binding.btnReady.setAlpha(1.0f);
             binding.btnOffline.setAlpha(0.5f);
         } else {
-            binding.tvStatusDescription.setText(com.drc.aidbridge.R.string.volunteer_dashboard_status_offline_placeholder);
+            binding.tvStatusDescription
+                    .setText(com.drc.aidbridge.R.string.volunteer_dashboard_status_offline_placeholder);
             binding.viewStatusIndicator.setBackgroundResource(com.drc.aidbridge.R.drawable.bg_circle_status_offline);
             binding.btnReady.setAlpha(0.5f);
             binding.btnOffline.setAlpha(1.0f);
---
+++ drc-app/app/src/main/java/com/drc/aidbridge/ui/main/fragment/volunteer/VolunteerSosAcceptanceFragment.java
@@ -0,0 +1,125 @@
+package com.drc.aidbridge.ui.main.fragment.volunteer;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.drc.aidbridge.R;
+import com.drc.aidbridge.databinding.FragmentVolunteerSosAcceptanceBinding;
+import com.drc.aidbridge.ui.base.BaseFragment;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class VolunteerSosAcceptanceFragment extends BaseFragment<FragmentVolunteerSosAcceptanceBinding> {
+
+    public static final String ARG_MISSION_TYPE = "missionType";
+    public static final String MISSION_TYPE_RESCUE = "RESCUE";
+    public static final String MISSION_TYPE_SUPPLY = "SUPPLY";
+
+    private static final long TIMER_TOTAL_MS = 60_000L;
+    private static final long TIMER_INTERVAL_MS = 1_000L;
+
+    private String missionType = MISSION_TYPE_RESCUE;
+    private CountDownTimer missionCountDownTimer;
+
+    @NonNull
+    public static VolunteerSosAcceptanceFragment newInstance(@NonNull String missionType) {
+        VolunteerSosAcceptanceFragment fragment = new VolunteerSosAcceptanceFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_MISSION_TYPE, missionType);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            String argType = getArguments().getString(ARG_MISSION_TYPE);
+            if (argType != null) {
+                missionType = argType;
+            }
+        }
+    }
+
+    @Override
+    protected FragmentVolunteerSosAcceptanceBinding inflateBinding(LayoutInflater inflater,
+            @Nullable ViewGroup container) {
+        return FragmentVolunteerSosAcceptanceBinding.inflate(inflater, container, false);
+    }
+
+    @Override
+    protected void setupViews() {
+        applyMissionTypeUI();
+        setupClickListeners();
+        startCountDownTimer();
+    }
+
+    @Override
+    protected void observeViewModel() {
+        // TODO: Inject ViewModel và observe LiveData để cập nhật dữ liệu nhiệm vụ từ
+        // UseCase
+    }
+
+    @Override
+    public void onDestroyView() {
+        cancelCountDownTimer();
+        super.onDestroyView();
+    }
+
+    private void applyMissionTypeUI() {
+        if (MISSION_TYPE_SUPPLY.equalsIgnoreCase(missionType)) {
+            binding.cardMissionBadge.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.hub_blue));
+            binding.tvMissionBadge.setText(R.string.volunteer_sos_acceptance_mission_badge_supply);
+            binding.tvMissionTitle.setText(R.string.volunteer_sos_acceptance_mission_title_supply);
+            return;
+        }
+
+        binding.cardMissionBadge.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.sos_red));
+        binding.tvMissionBadge.setText(R.string.volunteer_sos_acceptance_mission_badge);
+        binding.tvMissionTitle.setText(R.string.volunteer_sos_acceptance_mission_title);
+    }
+
+    private void setupClickListeners() {
+        binding.btnDecline.setOnClickListener(v -> popBackStackSafely());
+
+        binding.btnAccept.setOnClickListener(v -> {
+            showToast(getString(R.string.volunteer_sos_acceptance_toast_accept_success));
+            // TODO:
+            // navigateSafely(R.id.action_volunteerSosAcceptanceFragment_to_volunteerMissionExecutionFragment)
+        });
+    }
+
+    private void startCountDownTimer() {
+        cancelCountDownTimer();
+
+        missionCountDownTimer = new CountDownTimer(TIMER_TOTAL_MS, TIMER_INTERVAL_MS) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long secondsLeft = millisUntilFinished / 1000L;
+                binding.tvTimerSecondsValue.setText(String.format("%02d", secondsLeft));
+            }
+
+            @Override
+            public void onFinish() {
+                binding.tvTimerSecondsValue.setText(R.string.volunteer_sos_acceptance_timer_minutes_value);
+                popBackStackSafely();
+            }
+        };
+
+        missionCountDownTimer.start();
+    }
+
+    private void cancelCountDownTimer() {
+        if (missionCountDownTimer != null) {
+            missionCountDownTimer.cancel();
+            missionCountDownTimer = null;
+        }
+    }
+}
